079256fd124f6e36353115db46a4b208254d80dc
behoanglong <behoanglong12ls@gmail.com>
2025-06-21 22:19:09 +0700

    Revert "Add files via upload"

--- admin.js
+++
@@ -1,2 +0,0 @@
-
-document.getElementById("orders").innerHTML = "<p>(Demo) Tính năng admin xử lý đơn hàng sau sẽ được cập nhật.</p>";
--- payment.js
+++
@@ -1,4 +0,0 @@
-
-function buy(name, price, link) {
-  alert("Chuyển khoản " + price + " đến STK 0335008361 - MA THI HIEN\nNội dung: " + name);
-}
